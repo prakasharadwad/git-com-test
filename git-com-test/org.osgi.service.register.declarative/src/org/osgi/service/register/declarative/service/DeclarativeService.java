81d1963cb51161615531c4e25368350c4f62153b
Prakash <prakasharadwad@gmail.com>
2017-04-01 18:39:12 -0300

    welcome to test

---
+++ git-com-test/org.osgi.service.register.declarative/src/org/osgi/service/register/declarative/service/DeclarativeService.java
@@ -0,0 +1,9 @@
+package org.osgi.service.register.declarative.service;
+//you are in fourth one
+//now you start your journey from ground
+//make any changes to round up
+public interface DeclarativeService {
+	
+	public void declarativeService();
+
+}
