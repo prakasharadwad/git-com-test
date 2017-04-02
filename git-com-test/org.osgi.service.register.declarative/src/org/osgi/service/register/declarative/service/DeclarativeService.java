22af3a679234051c64ae125a7bceaca4eda61871
Prakash <prakasharadwad@gmail.com>
2017-04-02 11:41:40 -0300

    final changes

--- git-com-test/org.osgi.service.register.declarative/src/org/osgi/service/register/declarative/service/DeclarativeService.java
+++ git-com-test/org.osgi.service.register.declarative/src/org/osgi/service/register/declarative/service/DeclarativeService.java
@@ -2,6 +2,7 @@ package org.osgi.service.register.declarative.service;
 //you are in fourth one
 //now you start your journey from ground
 //make any changes to round up
+//come to point
 public interface DeclarativeService {
 	
 	public void declarativeService();
